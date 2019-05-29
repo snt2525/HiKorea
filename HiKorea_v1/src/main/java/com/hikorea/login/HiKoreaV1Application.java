dfccae0f5d7c734d4a67eae817754bf50ee3163d
나밍 <40882598+snt2525@users.noreply.github.com>
2019-05-29 22:38:34 +0900

    남희)하이 코리아 start!

---
+++ HiKorea_v1/src/main/java/com/hikorea/login/HiKoreaV1Application.java
@@ -0,0 +1,13 @@
+package com.hikorea.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiKoreaV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiKoreaV1Application.class, args);
+	}
+
+}
